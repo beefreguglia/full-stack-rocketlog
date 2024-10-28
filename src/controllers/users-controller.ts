480f83749ff0569e1e112bb9de304b4683b5717c
Bernardo <beefreguglia@gmail.com>
2024-10-28 13:41:33 -0300

    feature: create user route validation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 class UsersController {
   create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+
+    const { 
+      email, 
+      name, 
+      password 
+    } = bodySchema.parse(request.body)
+
+
     return response.json({ message: 'Ok' })
   }
 }
