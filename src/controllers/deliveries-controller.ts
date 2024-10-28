77b34e0f056b275f7fe3b6b68c7d0e471f194d2b
Bernardo <beefreguglia@gmail.com>
2024-10-28 20:52:34 -0300

    feature: create delivery create controller

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,24 @@
+import { prisma } from '@/database/prisma'
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+  async  create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    })
+
+    const { description, user_id } = bodySchema.parse(request.body)
+
+    await prisma.delivery.create({
+      data: {
+        userID: user_id,
+        description,
+      }
+    })
+
+    return response.status(201).json()
   }
 }
 
