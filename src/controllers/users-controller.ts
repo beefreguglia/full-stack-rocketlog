e67abb6ea4c2ece4daf639abfe6fcee4261c1618
Bernardo <beefreguglia@gmail.com>
2024-10-28 14:01:42 -0300

    feature: create users create controller

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,12 @@
+import { hash } from 'bcrypt'
 import { Request, Response } from 'express'
 import { z } from 'zod'
 
+import { prisma } from '@/database/prisma'
+import { AppError } from '@/utils/app-error'
+
 class UsersController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     const bodySchema = z.object({
       name: z.string().trim().min(3),
       email: z.string().email(),
@@ -14,9 +18,29 @@ class UsersController {
       name, 
       password 
     } = bodySchema.parse(request.body)
+    
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: { email }
+    })
 
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists")
+    }
 
-    return response.json({ message: 'Ok' })
+    const hashedPassword = await hash(password, 8)
+    
+    const { 
+      password: _,
+      ...userWithoutPassword
+    } = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword
+      }
+    })
+
+    return response.status(201).json(userWithoutPassword)
   }
 }
 
