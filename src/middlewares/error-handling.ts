9c4893050741ce35fbf190730fcc5c9df4dc2344
Bernardo <beefreguglia@gmail.com>
2024-10-28 12:01:28 -0300

    feature: handle validation errors with zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,5 +1,6 @@
 import { AppError } from '@/utils/app-error'
 import { Request, Response, NextFunction } from 'express'
+import { ZodError } from 'zod'
 
 export function errorHandling(
   error: any,
@@ -11,5 +12,11 @@ export function errorHandling(
     return response.status(error.statusCode).json({ message: error.message })
   }
 
+  if(error instanceof ZodError) {
+    return response.status(400).json({
+      message: "Validation error.",
+      issues: error.format(),
+    })
+  }
   return response.status(500).json({ message: error.message })
 }
